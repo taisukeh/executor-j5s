2bacfae49f2987504298f3a79909e25eb902c79f
taisukeh <taisuke.hori@gmail.com>
2017-07-26 20:50:14 +0900

    Docker, wait for job stop, more config, docs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Executor = require('screwdriver-executor-base');
 const path = require('path');
 const jenkins = require('jenkins');
 const fs = require('fs');
+const xmlescape = require('xml-escape');
+const shellescape = require('shell-escape');
 const Breaker = require('circuit-fuses');
 
 class J5sExecutor extends Executor {
@@ -18,10 +20,10 @@ class J5sExecutor extends Executor {
      * @param  {Function} callback           fn(err) from jenkinsClient
      */
     _jenkinsCommand(options, callback) {
-        options.params.push(callback);
+        // To pass arguments as an array, we need to use apply
         this.jenkinsClient[options.module][options.action].apply(
             this.jenkinsClient[options.module],
-            options.params);
+            options.params.concat([callback]));
     }
 
     /**
@@ -56,6 +58,193 @@ class J5sExecutor extends Executor {
     }
 
     /**
+     * Stop Jenkins job
+     * @method _jenkinsJobStop
+     * @param  {String}   jobName            Jenkins job name
+     * @return {Promise}
+     */
+    _jenkinsJobStop(jobName) {
+        return this.breaker.runCommand({
+            module: 'job',
+            action: 'get',
+            params: [{ name: jobName }]
+        }).then((data) => {
+            if (!(data && data.lastBuild && data.lastBuild.number)) {
+                throw new Error('No build has been started yet, try later');
+            }
+
+            return this.breaker.runCommand({
+                module: 'build',
+                action: 'stop',
+                params: [{ name: jobName, number: data.lastBuild.number }]
+            });
+        }).then(() => this._jenkinsJobWaitStop(jobName, 0));
+    }
+
+    /**
+     * Wait until the job stopped
+     * @method _jenkinsJobWaitStop
+     * @param  {String}   jobName            Jenkins job name
+     * @param  {Number}   timeConsumed       Elapsed time
+     * @return {Promise}
+     */
+    _jenkinsJobWaitStop(jobName, timeConsumed) {
+        if (timeConsumed >= this.cleanupTimeLimit) {
+            return Promise.resolve();
+        }
+
+        return this.breaker.runCommand({
+            module: 'job',
+            action: 'get',
+            params: [{ name: jobName }]
+        }).then((data) => {
+            if (data && data.lastCompletedBuild && data.lastCompletedBuild.number) {
+                return null;
+            }
+
+            // Sleep between retry
+            return new Promise((resolve) => {
+                setTimeout(() => resolve(), this.cleanupWatchInterval * 1000);
+            }).then(() =>
+                this._jenkinsJobWaitStop(jobName, timeConsumed + this.cleanupWatchInterval)
+            );
+        });
+    }
+
+    /**
+     * Jenkins job config xml
+     * @method _loadJobXml
+     * @param  {Object}   config        A configuration object psssed to _start
+     * @return {Promise}
+     */
+    _loadJobXml(config) {
+        const { buildScript, cleanupScript } = this._taskScript(config);
+
+        return new Promise((resolve, reject) => {
+            const configPath = path.resolve(__dirname, './config/job.xml');
+
+            fs.readFile(configPath, 'utf-8', (err, fileContents) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                return resolve(fileContents);
+            });
+        }).then(xml => (
+            xml.replace('<%= buildScript %>', xmlescape(buildScript))
+               .replace('<%= cleanupScript %>', xmlescape(cleanupScript))
+        ));
+    }
+
+    /**
+     * Script for job
+     * @method _taskScript
+     * @param  {Object}   config        A configuration object psssed to _start
+     * @return {Object}
+     */
+    _taskScript(config) {
+        if (this.buildScript && this.cleanupScript) {
+            return {
+                buildScript: this.buildScript,
+                cleanupScript: this.cleanupScript
+            };
+        }
+
+        return this._dockerTaskScript(config);
+    }
+
+    /**
+     * Script for job using docker
+     * @method _dockerTaskScript
+     * @param  {Object}   config        A configuration object psssed to _start
+     * @return {Object}
+     */
+    _dockerTaskScript(config) {
+        const pullLauncherImage = shellescape([
+            this.dockerCommand, 'pull', `screwdrivercd/launcher:${this.launchVersion}`
+        ]);
+        const pullBuildImage = shellescape([
+            this.dockerCommand, 'pull', config.container
+        ]);
+        const launcherContainerName = `${this.prefix}${config.buildId}-init`;
+        const createLauncherContainer = shellescape([
+            this.dockerCommand, 'run',
+            '--name', launcherContainerName,
+            '--label', `sdbuild=${this.prefix}${config.buildId}`,
+            '--entrypoint', '/bin/true',
+            `screwdrivercd/launcher:${this.launchVersion}`
+        ]);
+        const buildContainerName = `${this.prefix}${config.buildId}-build`;
+        const createBuildContainer = shellescape([
+            this.dockerCommand, 'run',
+            '--name', buildContainerName,
+            '--label', `sdbuild=${this.prefix}${config.buildId}`,
+            '--entrypoint', '/opt/sd/tini',
+            '--memory', this.memory,
+            '--memory-swap', this.memoryLimit,
+            '--volumes-from', `${launcherContainerName}:rw`,
+            '-e', 'SD_TOKEN',
+            config.container,
+            '--',
+            // Run a shell command
+            '/bin/sh',
+            '-c',
+            [
+                // Run the launcher in the background
+                '/opt/sd/launch',
+                '--api-uri',
+                this.ecosystem.api,
+                '--emitter',
+                '/opt/sd/emitter',
+                config.buildId,
+                '&',
+                // Run the logservice in the background
+                '/opt/sd/logservice',
+                '--emitter',
+                '/opt/sd/emitter',
+                '--api-uri',
+                this.ecosystem.store,
+                '--build',
+                config.buildId,
+                '&',
+                // Wait for both background jobs to complete
+                'wait $(jobs -p)'
+            ].join(' ')
+        ]);
+
+        const waitContainerEnd = shellescape([
+            this.dockerCommand, 'wait', buildContainerName
+        ]);
+
+        const buildScript = `
+set -eu
+
+${pullLauncherImage} &
+${pullBuildImage} &
+
+wait $(jobs -p)
+
+${createLauncherContainer}
+${createBuildContainer}
+
+${waitContainerEnd}
+`;
+
+        const stopContainer = shellescape([
+            this.dockerCommand, 'rm',
+            '-v', '-f',
+            buildContainerName,
+            launcherContainerName
+        ]);
+
+        const cleanupScript = `
+${stopContainer}
+`;
+
+        return { buildScript, cleanupScript };
+    }
+
+    /**
      * Jenkins job name
      * @method _jobName
      * @param  {String}   buildId     ID for the build
@@ -69,22 +258,42 @@ class J5sExecutor extends Executor {
      * Constructor
      * @method constructor
      * @param  {Object} options           Configuration options
-     * @param  {Object} options.ecosystem                        Screwdriver Ecosystem
-     * @param  {Object} options.ecosystem.api                    Routable URI to Screwdriver API
-     * @param  {Object} options.ecosystem.store                  Routable URI to Screwdriver Store
-     * @param  {String} options.host      Jenkins hostname to make requests to
-     * @param  {String} options.port      Jenkins port to make requests to
-     * @param  {String} options.username  Jenkins username
-     * @param  {String} options.password  Jenkins password/token
-     * @param  {String} [options.fusebox]                            Options for the circuit breaker (https://github.com/screwdriver-cd/circuit-fuses)
+     * @param  {Object} options.ecosystem                              Screwdriver Ecosystem
+     * @param  {Object} options.ecosystem.api                          Routable URI to Screwdriver API
+     * @param  {Object} options.ecosystem.store                        Routable URI to Screwdriver Store
+     * @param  {String} options.jenkins.host                           Jenkins hostname to make requests to
+     * @param  {Number} [options.jenkins.port=8080]                    Jenkins port to make requests to
+     * @param  {String} [options.jenkins.username='screwdriver']       Jenkins username
+     * @param  {String} options.jenkins.password                       Jenkins password/token
+     * @param  {String} [options.docker.command='docker']              Path to the docker command
+     * @param  {String} [options.docker.launchVersion='stable']        Launcher container version to use
+     * @param  {String} [options.docker.prefix='']                     Prefix to all container names
+     * @param  {String} [options.memory='4g']                          Memory limit (docker run `--memory` option)
+     * @param  {String} [options.memoryLimit='6g']                     Memory limit include swap (docker run `--memory-swap` option)
+     * @param  {String} [options.buildScript]                          Shell script to start the job
+     * @param  {String} [options.cleanupScript='']                     Shell script to clean up the job
+     * @param  {Number} [options.cleanupTimeLimit=20]                  Time to destory the job(seconds)
+     * @param  {Number} [options.cleanupWatchInterval=2]               Interval to detect the stopped job (seconds)
+     * @param  {String} [options.fusebox]                              Options for the circuit breaker (https://github.com/screwdriver-cd/circuit-fuses)
      */
     constructor(options) {
         super();
         this.ecosystem = options.ecosystem;
-        this.host = options.host;
-        this.port = options.port;
-        this.username = options.username;
-        this.password = options.password;
+        this.host = options.jenkins.host;
+        this.port = options.jenkins.port || '8080';
+        this.username = options.jenkins.username || 'screwdriver';
+        this.password = options.jenkins.password;
+        this.dockerCommand = (options.docker && options.docker.command) || 'docker';
+        this.launchVersion = (options.docker && options.docker.launchVersion) || 'stable';
+        this.prefix = (options.docker && options.docker.prefix) || '';
+        this.memory = (options.docker && options.docker.memory) || '4g';
+        this.memoryLimit = (options.docker && options.docker.memoryLimit) || '6g';
+
+        this.buildScript = options.buildScript;
+        this.cleanupScript = options.cleanupScript || '';
+        this.cleanupTimeLimit = options.cleanupTimeLimit || 20;
+        this.cleanupWatchInterval = options.cleanupWatchInterval || 2;
+
         // need to pass port number in the future
         this.baseUrl = `http://${this.username}:${this.password}@${this.host}:${this.port}`;
         this.jenkinsClient = jenkins({
@@ -108,17 +317,7 @@ class J5sExecutor extends Executor {
     _start(config) {
         const jobName = this._jobName(config.buildId);
 
-        return new Promise((resolve, reject) => {
-            const configPath = path.resolve(__dirname, './config/job.xml');
-
-            fs.readFile(configPath, 'utf-8', (err, fileContents) => {
-                if (err) {
-                    return reject(err);
-                }
-
-                return resolve(fileContents);
-            });
-        }).then(xml =>
+        return this._loadJobXml(config).then(xml =>
             this._jenkinsJobCreateOrUpdate(jobName, xml)
         ).then(() => {
             const parameters = {
@@ -147,21 +346,9 @@ class J5sExecutor extends Executor {
     _stop(config) {
         const jobName = this._jobName(config.buildId);
 
-        return this.breaker.runCommand({
-            module: 'job',
-            action: 'get',
-            params: [{ name: jobName }]
-        }).then((data) => {
-            if (!(data && data.lastBuild && data.lastBuild.number)) {
-                throw new Error('No build has been started yet, try later');
-            }
-
-            return this.breaker.runCommand({
-                module: 'build',
-                action: 'stop',
-                params: [{ name: jobName, number: data.lastBuild.number }]
-            });
-        }).then(() => this.breaker.runCommand({
+        return this._jenkinsJobStop(
+            jobName
+        ).then(() => this.breaker.runCommand({
             module: 'job',
             action: 'destroy',
             params: [{ name: jobName }]
--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,9 @@ const sinon = require('sinon');
 const mockery = require('mockery');
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
+const xmlescape = require('xml-escape');
+const shellescape = require('shell-escape');
 
 sinon.assert.expose(assert, { prefix: '' });
 
@@ -18,6 +21,7 @@ describe('index', () => {
     let jenkinsMock;
     let breakerMock;
     let BreakerFactory;
+    let compiledJobXml;
 
     const config = {
         buildId: 1993,
@@ -52,7 +56,22 @@ describe('index', () => {
         }
     };
 
+    const fakeCompletedJobInfo = {
+        lastBuild: {
+            number: 1
+        },
+        lastCompletedBuild: {
+            number: 1
+        }
+    };
+
     const buildNumber = fakeJobInfo.lastBuild.number;
+
+    const buildScript = '/opt/bin/sd-build-test-<should-escape-xml>';
+
+    const cleanupScript = '/opt/bin/sd-cleanup-test-<should-escape-xml>';
+
+    const cleanupWatchInterval = 0.01;
 
     before(() => {
         mockery.enable({
@@ -89,9 +108,18 @@ describe('index', () => {
 
         executor = new Executor({
             ecosystem,
-            host: 'jenkins',
-            username: 'admin',
-            password: 'fakepassword'
+            jenkins: {
+                host: 'jenkins',
+                username: 'admin',
+                password: 'fakepassword'
+            },
+            buildScript,
+            cleanupScript,
+            cleanupWatchInterval
+        });
+        compiledJobXml = _.template(TEST_XML)({
+            buildScript: xmlescape(buildScript),
+            cleanupScript: xmlescape(cleanupScript)
         });
     });
 
@@ -121,13 +149,13 @@ describe('index', () => {
             createOpts = {
                 module: 'job',
                 action: 'create',
-                params: [{ name: jobName, xml: TEST_XML }]
+                params: [{ name: jobName, xml: compiledJobXml }]
             };
 
             configOpts = {
                 module: 'job',
                 action: 'config',
-                params: [{ name: jobName, xml: TEST_XML }]
+                params: [{ name: jobName, xml: compiledJobXml }]
             };
 
             existsOpts = {
@@ -252,6 +280,40 @@ describe('index', () => {
         });
 
         it('return null when the build is successfully stopped', (done) => {
+            breakerMock.runCommand.withArgs(getOpts).onCall(0).resolves(fakeJobInfo);
+            breakerMock.runCommand.withArgs(getOpts).onCall(1).resolves(fakeCompletedJobInfo);
+            breakerMock.runCommand.withArgs(stopOpts).resolves(null);
+            breakerMock.runCommand.withArgs(destroyOpts).resolves(null);
+
+            executor.stop(buildIdConfig).then((ret) => {
+                assert.isNull(ret);
+                assert.calledWith(breakerMock.runCommand, getOpts);
+                assert.calledWith(breakerMock.runCommand, stopOpts);
+                assert.calledWith(breakerMock.runCommand, destroyOpts);
+                done();
+            });
+        });
+
+        it('return null when the build is successfully stopped after a while', (done) => {
+            breakerMock.runCommand.withArgs(getOpts).onCall(0).resolves(fakeJobInfo);
+            breakerMock.runCommand.withArgs(getOpts).onCall(1).resolves(fakeJobInfo);
+            breakerMock.runCommand.withArgs(getOpts).onCall(2).resolves(fakeCompletedJobInfo);
+            breakerMock.runCommand.withArgs(stopOpts).resolves(null);
+            breakerMock.runCommand.withArgs(destroyOpts).resolves(null);
+
+            executor.stop(buildIdConfig).then((ret) => {
+                assert.isNull(ret);
+                assert.calledWith(breakerMock.runCommand, getOpts);
+                assert.calledWith(breakerMock.runCommand, stopOpts);
+                assert.calledWith(breakerMock.runCommand, destroyOpts);
+                done();
+            });
+        });
+
+        it('return null when the build is successfully stopped after cleanup timeout', (done) => {
+            // change executor's 'internal config
+            executor.cleanupTimeLimit = 0.05;
+
             breakerMock.runCommand.withArgs(getOpts).resolves(fakeJobInfo);
             breakerMock.runCommand.withArgs(stopOpts).resolves(null);
             breakerMock.runCommand.withArgs(destroyOpts).resolves(null);
@@ -302,6 +364,112 @@ describe('index', () => {
                 done();
             });
         });
+
+        it('return error when second job.get is getting error', (done) => {
+            const error = new Error('job.get error');
+
+            breakerMock.runCommand.withArgs(getOpts).onCall(0).resolves(fakeJobInfo);
+            breakerMock.runCommand.withArgs(getOpts).onCall(1).rejects(error);
+            breakerMock.runCommand.withArgs(stopOpts).resolves();
+
+            executor.stop(buildIdConfig).catch((err) => {
+                assert.deepEqual(err, error);
+                done();
+            });
+        });
+    });
+
+    describe('use docker', () => {
+        beforeEach(() => {
+            mockery.deregisterMock('circuit-fuses');
+            mockery.resetCache();
+
+            // eslint-disable-next-line global-require
+            Executor = require('../index');
+
+            executor = new Executor({
+                ecosystem,
+                jenkins: {
+                    host: 'jenkins',
+                    username: 'admin',
+                    password: 'fakepassword'
+                }
+            });
+
+            const taskScript = executor._dockerTaskScript(config);
+
+            compiledJobXml = _.template(TEST_XML)({
+                buildScript: xmlescape(taskScript.buildScript),
+                cleanupScript: xmlescape(taskScript.cleanupScript)
+            });
+
+            jenkinsMock.job.create = sinon.stub(executor.jenkinsClient.job, 'create');
+            jenkinsMock.job.config = sinon.stub(executor.jenkinsClient.job, 'config');
+            jenkinsMock.job.exists = sinon.stub(executor.jenkinsClient.job, 'exists');
+            jenkinsMock.job.build = sinon.stub(executor.jenkinsClient.job, 'build');
+        });
+
+        it('calls jenkins function correctly', (done) => {
+            fsMock.readFile.yieldsAsync(null, compiledJobXml);
+            jenkinsMock.job.exists.yieldsAsync(null, false);
+            jenkinsMock.job.create.yieldsAsync(null);
+            jenkinsMock.job.build.yieldsAsync(null);
+
+            executor.start(config).then(() => {
+                assert.calledWith(jenkinsMock.job.create, { name: jobName, xml: compiledJobXml });
+                assert.calledWith(jenkinsMock.job.build,
+                                  { name: jobName, parameters: buildParameters });
+                done();
+            });
+        });
+
+        it('run docker command with default options correctly', () => {
+            // build container
+            assert.include(compiledJobXml, xmlescape(shellescape(['--memory', '4g'])));
+            assert.include(compiledJobXml, xmlescape(shellescape(['--memory-swap', '6g'])));
+            assert.include(compiledJobXml, xmlescape(config.container));
+
+            // launcher container
+            assert.include(compiledJobXml,
+                           xmlescape(shellescape(['--label', `sdbuild=${config.buildId}`])));
+            assert.include(compiledJobXml, xmlescape('screwdrivercd/launcher:stable'));
+        });
+
+        it('run docker command with provided options correctly', () => {
+            const executorConfig = {
+                ecosystem,
+                jenkins: {
+                    host: 'jenkins',
+                    username: 'admin',
+                    password: 'fakepassword'
+                },
+                docker: {
+                    command: '/foo/docker',
+                    memory: '1g',
+                    memoryLimit: '2g',
+                    prefix: 'foo-prefix',
+                    launchVersion: 'foo-ver'
+                }
+            };
+
+            executor = new Executor(executorConfig);
+
+            const build = executor._dockerTaskScript(config).buildScript;
+
+            // build container
+            assert.include(build,
+                           shellescape(['--memory', executorConfig.docker.memory]));
+            assert.include(build,
+                           shellescape(['--memory-swap', executorConfig.docker.memoryLimit]));
+
+            // launcher container
+            const label = `sdbuild=${executorConfig.docker.prefix}${config.buildId}`;
+
+            assert.include(build,
+                           shellescape(['--label', label]));
+            assert.include(build,
+                           `screwdrivercd/launcher:${executorConfig.docker.launchVersion}`);
+        });
     });
 
     describe('run without Mocked Breaker', () => {
@@ -314,9 +482,13 @@ describe('index', () => {
 
             executor = new Executor({
                 ecosystem,
-                host: 'jenkins',
-                username: 'admin',
-                password: 'fakepassword'
+                jenkins: {
+                    host: 'jenkins',
+                    username: 'admin',
+                    password: 'fakepassword'
+                },
+                buildScript,
+                cleanupScript
             });
 
             jenkinsMock.job.create = sinon.stub(executor.jenkinsClient.job, 'create');
@@ -332,7 +504,7 @@ describe('index', () => {
             jenkinsMock.job.build.yieldsAsync(null);
 
             executor.start(config).then(() => {
-                assert.calledWith(jenkinsMock.job.create, { name: jobName, xml: TEST_XML });
+                assert.calledWith(jenkinsMock.job.create, { name: jobName, xml: compiledJobXml });
                 assert.calledWith(jenkinsMock.job.build,
                                   { name: jobName, parameters: buildParameters });
                 done();
